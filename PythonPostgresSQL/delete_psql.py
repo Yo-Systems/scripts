1c6a1cb8015f6f09c56358bb2ab4e4596b20af6d
DevOpsYoSystems <73576030+DevOpsYoSystems@users.noreply.github.com>
2020-11-20 00:13:19 -0600

    Add files via upload

---
+++ PythonPostgresSQL/delete_psql.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import psycopg2
+from config import config
+
+def delete_part(part_id):
+    """ delete part by part id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute("DELETE FROM parts WHERE part_id = %s", (part_id,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows_deleted
---
+++ PythonPostgresSQL/query_psql.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import psycopg2
+from config import config
+
+def get_vendors():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
